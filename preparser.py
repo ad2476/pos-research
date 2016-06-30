72fe3ab52e60d709b2c1abb5c0977daa244f82b1
Arun Drelich <ad2476@gmail.com>
2016-06-29 20:25:08 -0400

    Modularised parsing of tagged file format to allow for different formats

--- preparser.py
+++ preparser.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+import re
+import itertools
 from hmm import STOP
+
+stopPair = STOP + " " + STOP
 
 """ This class will pre-parse POS-tagged files in the format of the WSJ data """
 class EnglishWSJParser:
@@ -11,7 +16,6 @@ class EnglishWSJParser:
     words = []
     tags = []
 
-    stopPair = STOP + " " + STOP
     for line in self._rawdata:
       line = stopPair + " " + line + stopPair # pad with stop symbols
       sentence = line.split()
@@ -24,3 +28,47 @@ class EnglishWSJParser:
         tags.append(label)
 
     return words,tags
+
+  def readTestSentence(self, line):
+    return line.split()[::2]
+
+  def formatOutput(self, words, tags):
+    output = ""
+    for word,tag in itertools.izip(words, tags):
+      output += word + " " + tag + " "
+
+    return output
+
+class SanskritJNUParser:
+  
+  def __init__(self, inputData):
+    self._rawdata = inputData
+
+  def parseWordsTags(self):
+    words = []
+    tags = []
+
+    for line in self._rawdata:
+      # each line is formatted: "WORD[TAG] WORD[TAG] WORD[TAG]DANDA[TAG]\n"
+      
+      # capturing group before a literal '[' char:
+      #  match at least 0 times,lazy, on a group consisting of:
+      #   not whitespace, not a ']' char
+      words.extend(re.findall(r"([^\s\]]*?)\[", line))
+
+      tags.extend(re.findall(r"\[(.*?)\]", line)) # find everything within brackets
+
+    return words,tags
+
+  def readTestSentence(self, line):
+    return re.findall(r"([^\s\]]*?)\[", line)
+
+  def formatOutput(self, words, tags):
+    n = len(words)
+    output = ""
+    for i in xrange(0, n):
+      output += words[i] + "[" + tags[i] + "] "
+
+    return output
+
+    

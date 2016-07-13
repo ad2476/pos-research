cb7745e1c23c1913402c000f245c7dfc55e6c411
Arun Drelich <ad2476@gmail.com>
2016-07-12 21:31:40 -0400

    Hash words, map tags/states to indices, use numpy arrays where possible

--- scripts/hmm.py
+++ scripts/hmm.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import random
 import itertools
+import numpy as np
 
 STOP = "**@sToP@**" # The stop symbol
 
@@ -8,12 +9,24 @@ STOP = "**@sToP@**" # The stop symbol
 class HiddenDataHMM:
 
   """ Construct the HMM object using a list of outputs and a set of posLabels. """
-  def __init__(self, outputs, posLabels):
-    self._sentences = outputs # list of list of words, each elem is a sentence (as list)
-    self._states = posLabels
+  def __init__(self, outputs, posLabels, labelHash=None):
     self._ITER_CAP = 10
 
-    self._sigma = defaultdict(self._paramSmooth)
+    self._sentences = [[hash(x) for x in sentence] for sentence in outputs]
+    self._numStates = len(posLabels)
+    self._states = xrange(0,self._numStates) # faster np.array indexing
+
+    if not labelHash:
+      self._labelHash = {} # map actual label to its internal index number
+      for each in enumerate(posLabels):
+        i,y = each
+        self._labelHash[y] = i
+    else:
+      self._labelHash = labelHash
+
+    self._STOPTAG = self._labelHash[STOP] # which one is the stop tag?
+
+    self._sigma = np.full([self._numStates]*2, 0.01)*np.random.uniform(0.95,1.05) # [y,yprime]->prob.
     self._tau = defaultdict(self._paramSmooth)
 
     self.unkCount = 0 # used for metrics calculation (e.g. % UNK in doc)
@@ -24,41 +37,45 @@ class HiddenDataHMM:
   def _paramSmooth(pair):
     return 0.01*random.uniform(0.95,1.05)
 
-  """ Compute alpha for the current timestep
-        prevAlpha: dictionary of alpha(i-1): y->float
+  """ Compute alphas for this timestep.
+        alpha: n x m slice of the alphaBetaMat (n words by m states)
         x: the output at this timestep
-      Return: float (to be added to ith iteration's alpha(i) dict)
+      Return: Nothing, calculated in place
   """
-  def _computeAlpha(self, prevAlpha, x):
-    alpha = defaultdict(float)
-    if prevAlpha is None: # base case
-      alpha[STOP] = 1.0
-    else:
-      for y in self._states:
-        val = 0
-        for yprime in self._states:
-          val += prevAlpha[yprime]*self._sigma[(yprime,y)]
-        alpha[y] = val*self._tau[(y,x)]
+  def _computeAlphasTimestep(self, alpha, i, x):
+    for y in self._states:
+      val = 0.0
+      for yprime in self._states:
+        val += alpha[i-1][yprime]*self._sigma[yprime,y]
+      alpha[i][y] = val*self._tau[(y,x)]
 
-    return alpha
-
-  """ Compute beta for the current timestep
-        prevBeta: dictionary of beta(i+1): y->float
-        xNext: x_{i+1}, i.e. the output one position ahead of this timestep
-      Return: float
+  """ Compute alphas for this timestep.
+        beta: n x m slice of the alphaBetaMat (n words by m states)
+        xNext: the output at next timestep
+      Return: Nothing, calculated in place
   """
-  def _computeBeta(self, prevBeta, xNext):
-    beta = defaultdict(float)
-    if prevBeta is None:
-      beta[STOP] = 1.0
-    else:
-      for y in self._states:
-        val = 0
-        for yprime in self._states:
-          val += prevBeta[yprime]*self._sigma[(y,yprime)]*self._tau[(yprime,xNext)]
-        beta[y] = val
+  def _computeBetasTimestep(self, beta, i, xNext):
+    for y in self._states:
+      val = 0.0
+      for yprime in self._states:
+        val += beta[i+1][yprime]*self._sigma[y,yprime]*self._tau[(yprime,xNext)]
+      beta[i][y] = val
 
-    return beta
+  """ Not used rn, not compat with np structure!! """
+  def _forwardBackward(self, prevFwdBwd, x_i, x_j1):
+    prevAlpha, prevBeta = prevFwdBwd
+    alpha, beta = np.zeros(self._numStates), np.zeros(self._numStates)
+
+    for y in self._states:
+      alpha_val = 0
+      beta_val = 0
+      for yprime in self._states:
+        alpha_val += prevAlpha[yprime]*self._sigma[(yprime,y)]
+        beta_val += prevBeta[yprime]*self._sigma[(y,yprime)]*self._tau[(yprime,x_j1)]
+      alpha[y] = alpha_val*self._tau[(y,x_i)]
+      beta[y] = beta_val
+
+    return (alpha, beta)
 
   """ Compute E[n_{i,y,x}|x].
         alpha_y: alpha_y(i)
@@ -88,36 +105,42 @@ class HiddenDataHMM:
       self._sigma, self._tau = start_distribution
 
     iterations = 0
+    n_sentence = len(self._sentences)
     while iterations < self._ITER_CAP:
       print "iteration %i" % iterations
-      expected_yy_ = defaultdict(float) # E[n_{y,y'}|x]: (y,y')->float
+      expected_yy_ = np.zeros([self._numStates]*2) # E[n_{y,y'}|x]: (y,y')->float
       expected_yx = defaultdict(float) # E[n_{y,x}|x]: (y,x)->float
-      expected_ycirc = defaultdict(float) # E[n_{y,\circ}|x]: y->float
+      expected_ycirc = np.zeros(self._numStates) # E[n_{y,\circ}|x]: y->float
 
       # (E-step):
       s = 0
       for sentence in self._sentences:
-        print "- sentence: %i" % s
+        print "- sentence: %i of %i \t\t (iteration %i)" % (s, n_sentence, iterations)
         s+=1
 
         n = len(sentence)
-        alpha = [None for _ in range(0,n)]
-        beta = [None for _ in range(0,n)]
+        alpha = np.zeros([n, self._numStates])
+        beta = np.zeros([n, self._numStates])
 
-        alpha[0] = self._computeAlpha(None, STOP) # starts from the front
-        beta[n-1] = self._computeBeta(None, None) # starts from the back
+        ALPHA, BETA = 0, 1 # indices
+        alphaBetaMat = np.zeros([2, n, self._numStates]) # [alpha or beta][timestep][state] -> prob.
+
+        alphaBetaMat[ALPHA][0][self._STOPTAG] = 1.0
+        alphaBetaMat[BETA][n-1][self._STOPTAG] = 1.0
 
         # iterate over sentence without initial STOP for alpha, last STOP for beta
         # e.g. [STOP, "hello", "world", STOP]
         # Calculate alpha and beta using our sigmas and taus
         print "-- compute alpha and beta:"
+        alphas = alphaBetaMat[ALPHA,:,:]
+        betas = alphaBetaMat[BETA,:,:]
         for i in xrange(1,n):
           print "--- word %i" % i
           j = n - i - 1
           x_i = sentence[i]
           x_j1 = sentence[j+1]
-          alpha[i] = self._computeAlpha(alpha[i-1], x_i)
-          beta[j] = self._computeBeta(beta[j+1], x_j1)
+          self._computeAlphasTimestep(alphas, i, x_i) # compute alphas for this timestep
+          self._computeBetasTimestep(betas, j, x_j1) # compute betas for this timestep
 
         # Here we go again, now to calculate expectations
         print "-- compute expectations:"
@@ -126,23 +149,23 @@ class HiddenDataHMM:
           x = sentence[i]
           nextX = sentence[i+1]
           for y in self._states:
-            alpha_y = alpha[i][y]
-            beta_y = beta[i][y]
-            totalProb = alpha[n-1][STOP]
+            alpha_y = alphas[i][y]
+            beta_y = betas[i][y]
+            totalProb = alphas[n-1][self._STOPTAG]
             expOutputFreq = self._expEmissionFreq(alpha_y, beta_y, totalProb)
             expected_yx[(y,x)] += expOutputFreq
-            expected_ycirc[y] += expOutputFreq # TODO: idk if this is right??
+            expected_ycirc[y] += expOutputFreq
 
-            for y_ in self._states:
+            for y_ in self._states: # iterate over y' for E[n_{y,y'}|x]
               beta_y_ = beta[i+1][y_]
-              sigma = self._sigma[(y,y_)]
+              sigma = self._sigma[y,y_]
               tau = self._tau[(y_,nextX)]
-              expected_yy_[(y,y_)] += self._expTransitionFreq(alpha_y,beta_y_,sigma,tau,totalProb)
+              expected_yy_[y,y_] += self._expTransitionFreq(alpha_y,beta_y_,sigma,tau,totalProb)
 
       # (M-step): update sigma and tau
-      for transition,expectation in expected_yy_.iteritems():
-        y, _ = transition
-        self._sigma[transition] = expectation/expected_ycirc[y]
+      for y in self._states:
+        for yprime in self._states:
+          self._sigma[y,yprime] = expected_yy_[y,yprime]/expected_ycirc[y]
 
       for emission,expectation in expected_yx.iteritems():
         y, _ = emission
@@ -151,13 +174,16 @@ class HiddenDataHMM:
       iterations += 1 # increment iterations count
 
   def getSigma(self, y, yprime):
-    return self._sigma[(y,yprime)]
+    y = self._labelHash[y]
+    yprime = self._labelHash[yprime]
+    return self._sigma[y,yprime]
 
   def getTau(self, y, x):
-    return self._tau[(y,x)]
+    y = self._labelHash[y]
+    return self._tau[(y,hash(x))]
 
   def getLabels(self):
-    return self._states
+    return self._states # these are hashed
 
 """ A Hidden Markov Model constructed from visible data """
 class VisibleDataHMM:
@@ -238,5 +264,5 @@ class VisibleDataHMM:
 
   """ Return a copy of the labels of this HMM """
   def getLabels(self):
-    return list(self.n_ycirc.keys())
+    return set(self.n_ycirc.keys())
 

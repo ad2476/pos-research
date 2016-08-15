0b0a3fcfd922093c6ef43dac1110d24847537ff5
Arun Drelich <ad2476@gmail.com>
2016-08-15 18:30:31 -0400

    Renamed unker to unk, fixed some unking bugs

--- scripts/hmm/unk.py
+++ scripts/hmm/unk.py
@@ -8,6 +8,9 @@ class AbstractUnker:
         counts: a dictionary mapping word->count
         unk_thresh: threshold count for when a word should be UNKed
                     i.e. only substitute UNK for words with count<=unk_thresh
+
+      The corpus is processed in this __init__ method, which will take approx. O(n) time
+       for n words in the corpus.
   """
   def __init__(self, corpus, counts, unk_thresh=1):
     self._corpus = corpus
@@ -15,10 +18,14 @@ class AbstractUnker:
     self._thresh = unk_thresh
 
     # _abstractGuard() should init self._rulesList in subclasses
-    self._ruleList = None
+    self._rulesList = None
+
+    self._unkedCorpus = None # initialised in _processCorpus()
 
     self._abstractGuard() # raise error only if self is AbstractUnker but not a subclass
     self._rulesetHead = listToRuleset(self._rulesList)
+    self._processCorpus()
+
 
   """ OVERRIDE THIS IN ALL SUBCLASSES!! """
   def _abstractGuard(self):
@@ -27,23 +34,23 @@ class AbstractUnker:
   """ Given a word that should be substituted for UNK, evaluate
       which category of UNK applies based on this Unker's rules
   """
-  def categoriseUnk(self, word):
+  def _categoriseUnk(self, word):
     ruleset = self._rulesetHead # get the list of rules we need to apply
 
     # while there are still rules to evaluate:
-    word = "*U*" # catchall UNK category
+    unk = "*U*" # catchall UNK category
     while ruleset is not None:
       rule = ruleset.getRule()
       if rule.appliesTo(word):
-        word = rule.execute()
+        unk = rule.execute()
         break # stop at first applicable UNK substitution
 
       ruleset = ruleset.nextRule() # otherwise, keep looking
 
-    return word
+    return unk
 
-  """ Process the corpus and return a corpus with UNKs substituted """
-  def processCorpus(self):
+  """ Process the corpus, internally store the corpus w. UNK substitutions """
+  def _processCorpus(self):
     res = [] # will be a copy of the original corpus but with any UNK substitutions
 
     for line in self._corpus:
@@ -52,13 +59,40 @@ class AbstractUnker:
       for word in line:
         # substitute UNK only if below threshold
         if self._counts[word] <= self._thresh:
-          word = self.categoriseUnk(word)
-          
+          before = word
+          word = self._categoriseUnk(word)
+          print before, word
+
         newl.append(word) # append this word (poss. with sub.)
 
       res.append(newl) # append this line to the resulting list
 
-    return res
+    self._unkedCorpus = res
+
+  """ Check if a word is not in the dictionary (i.e. it is unknown)
+       and return the properly Unked form. Otherwise return the word.
+  """
+  def evaluateWord(self, word):
+    if word not in self._counts:
+      return self._categoriseUnk(word)
+
+    return word
+
+  """ Return the corpus with UNKs substituted as necessary """
+  def getUnkedCorpus(self):
+    return self._unkedCorpus
+
+  """ Return the original word found in the jth position of the ith
+       sentence of the unmodified corpus.
+       e.g. getOrigWord(0,1) will return "world"
+            for corpus=[["hello", "world"],["demo", "example"]]
+  """
+  def getOrigWord(self, i, j):
+    return self._corpus[i][j]
+
+  """ Return the original corpus without any UNK substitutions """
+  def getOrigCorpus(self):
+    return self._corpus
 
 """ Unker for Sanskrit that uses a rudimentary knowledge of Sanskrit grammar
     to place low-count words into UNK categories based on commonly-seen morphologies
@@ -74,25 +108,25 @@ class PratyayaUnker(AbstractUnker):
     _asya, Uasya = "asya", "*Uasya*" # 6. vibhakti sing. -a stem
     _ena, _eNa, Uena = "ena", "e\xe1\xb9\x87a", "*Uena*" # 3. vibhakti sing. -a stem
     _Su, _su, Usu = "\xe1\xb9\xa3u", "su", "*Usu*" # 7. vibhakti plu. -su/-ṣu represent the same form
-    _as, Uas = "a\xe1\xb8\xa5", "*Uas*" # 1s/1p/2p/5s/6s/etc -aḥ is a common inflectional ending
-    _As, UAs = "\xc4\x81\xe1\xb8\xa5", "*UAs*" # 1p/etc -āḥ also a common inflectional ending
-    _am, _aM, Uam = "am", "a\xe1\xb9\x83", "*Uam*" # 1s/2s
-    _Am, _AM, UAm = "\xc4\x81m", "\xc4\x81\xe1\xb9\x83", "*UAm*" # 2s/6p/etc
+    _as, Uas = "ah\xcc\xa3", "*Uas*" # 1s/1p/2p/5s/6s/etc -aḥ is a common inflectional ending
+    _As, UAs = "\xc4\x81h\xcc\xa3", "*UAs*" # 1p/etc -āḥ also a common inflectional ending
+    _am, _aM, Uam = "am", "am\xcc\xa3", "*Uam*" # 1s/2s
+    _Am, _AM, UAm = "\xc4\x81m", "\xc4\x81m\xcc\xa3", "*UAm*" # 2s/6p/etc
     _au, Uau = "au", "*Uau*" # 1d/2d/etc
-    _is, Uis = "i\xe1\xb8\xa5", "*Uis*"
-    _os, Uos = "o\xe1\xb8\xa5", "*Uos*"
+    _is, Uis = "ih\xcc\xa3", "*Uis*"
+    _os, Uos = "oh\xcc\xa3", "*Uos*"
     _At, UAt = "\xc4\x81t", "*UAt*" # 5. vibhakti sing. -a stem
     _at, Uat = "at", "*Uat*" # mostly seen in parasmai-/ātmanepada 3s verb forms?
     _vA, _tyA, UvA = "v\xc4\x81", "ty\xc4\x81", "*UvA*" # gerunds often end in -vā or -tyā
     _A, UA = "\xc4\x81", "*UA*" # e.g. kanyā, rājā, etc.
     _e, Ue = "e", "*Ue*" # 7. vibh. sing. -a stem, or vocatives etc
-    _s, Us = "\xe1\xb8\xa5", "*Us*" # consonant stem ending -ḥ
+    _s, Us = "h\xcc\xa3", "*Us*" # consonant stem ending -ḥ
     _i, Ui = "i", "*Ui*" # 7. vibh. sing.
     _I, UI = "\xc4\xab", "*UI*" # 1. vibh. sing. fem.
 
-    wordEnds = lambda w,s: word[-len(s):] == s
+    wordEnds = lambda w,s: w[-len(s):] == s
 
-    self._ruleList = [(lambda w: wordEnds(w, _asya), Uasya),
+    self._rulesList = [(lambda w: wordEnds(w, _asya), Uasya),
                       (lambda w: wordEnds(w, _ena) or wordEnds(w, _eNa), Uena),
                       (lambda w: wordEnds(w, _su) or wordEnds(w, _Su), Usu),
                       (lambda w: wordEnds(w, _as), Uas),
@@ -122,5 +156,5 @@ class BasicUnker(AbstractUnker):
   def _abstractGuard(self):
     # technically this is redundant, since categoriseUnk() will default to "*U*" if
     #  no other categories apply
-    self._ruleList = [(lambda w: True, "*U*")]
+    self._rulesList = [(lambda w: True, "*U*")]
 
